f24d09b5955b75a2a3826c5d08b5372fd824000e
Patrick Farrell <mr.patfarrell@gmail.com>
2023-02-13 15:18:54 -0500

    script to update known item records

---
+++ bin/domains.py
@@ -0,0 +1,77 @@
+from datetime import datetime
+import sys
+import sqlite3
+from urllib.parse import urlparse
+
+cached_hosts = {}
+new_hosts = {"cnt": 0}
+
+
+def warm_cache(conn):
+    cur = conn.cursor()
+    cur.execute("SELECT id, name from host")
+    rows = cur.fetchall()
+    for row in rows:
+        cached_hosts[row[1]] = row[0]
+    
+
+def get_db(dbfile):
+    conn = None
+    try:
+        conn = sqlite3.connect(dbfile)
+    except Error as e:
+        print(e)
+
+    return conn
+
+
+def get_unprocessed_hosts(conn, limit=5000):
+    cur = conn.cursor()
+    cur.execute("SELECT id, url from item where url is not null and host_id is null limit :limit", {'limit': limit})
+    rows = cur.fetchall()
+    return rows
+
+
+def find_or_create_host(conn, host):
+    id = cached_hosts.get(host, None)
+    if not id:
+        cur = conn.cursor()
+        cur.execute("INSERT INTO host(name) VALUES (:name) RETURNING id", {'name': host})  
+        rows = cur.fetchall()
+        id = rows[0][0]
+        cached_hosts[host] = id
+        new_hosts["cnt"] += 1
+    return id
+#    cur = conn.cursor()
+#    rows = cur.execute("SELECT id from host where name=:name", {'name': host}).fetchall()
+#    if not rows:
+#        cur.execute("INSERT INTO host(name) VALUES (:name) RETURNING id", {'name': host})  
+#        rows = cur.fetchall()
+#    return rows[0][0]    
+
+
+def update_item(conn, id, url):
+    host = urlparse(url).netloc
+    host_id = find_or_create_host(conn, host)
+    sql = "UPDATE item set host_id=:host_id WHERE id=:id"
+    data = { 'host_id': host_id, 'id': id} 
+    cur = conn.cursor()
+    cur.execute(sql, data)
+    conn.commit()
+
+
+if __name__ == '__main__':
+    dbfile = 'instance/hn.db'
+    conn = get_db(dbfile)
+    warm_cache(conn)
+    print("start: " + str(datetime.now()))
+    import ipdb
+    for i in range(0, 1000):
+        rows =  get_unprocessed_hosts(conn)
+        if len(rows) == 0:
+            break;
+        for row in rows:
+            update_item(conn, row[0], row[1])
+            
+        print(str(i) + ": " + str(datetime.now()) + " " + str(new_hosts['cnt']) + " new host entries")
+        new_hosts["cnt"] = 0
--- bin/rectify.py
+++ bin/rectify.py
@@ -22,16 +22,29 @@ def get_dates(datestr):
     return dt.timestamp(), maxDt.timestamp()
 
 
-def get_min_id(conn, dt):
+def get_min_id(conn, dt, mode):
     cur = conn.cursor()
-    cur.execute(f"SELECT id from item where time > :date and type='story' and rectified IS NULL order by time limit 1", {'date': dt})
+    data = {'date': dt }
+    if mode == 'unprocessed':
+        sql = "SELECT id from item where time > :date and type='story' and rectified IS NULL order by time limit 1"
+    elif mode == 'stories':
+        sql = "SELECT id from item where time > :date and type='story' order by time limit 1"
+    else:
+        sql = "SELECT id from item where time > :date order by time limit 1"
+
+    cur.execute(sql, data)
     rows = cur.fetchall()
     return rows[0][0]
 
 
-def get_max_id(conn, dt):
+def get_max_id(conn, dt, mode):
     cur = conn.cursor()
-    cur.execute(f"SELECT id from item where time < :date and type='story' and rectified is NULL  order by time desc limit 1", {'date': dt})
+    data = {'date': dt }
+    if mode != 'all':
+        sql = "SELECT id from item where time < :date and type='story' and rectified IS NULL order by time desc limit 1"
+    else:
+        sql = "SELECT id from item where time < :date order by time desc limit 1"
+    cur.execute(sql, data)
     rows = cur.fetchall()
     return rows[0][0]
 
@@ -73,16 +86,23 @@ def update_item(conn, id, json, capture_time):
 
 
 if __name__ == '__main__':
+    if(len(sys.argv) < 2):
+        print(f"usage: python bin/rectify.py [yyyy-mm-dd] ([all])")
+        sys.exit(1)   
+    mode = "stories" if len(sys.argv) == 2 else sys.argv[2]
+    minDate, maxDate = get_dates(sys.argv[1])
     dbfile = 'instance/hn.db'
     conn = get_db(dbfile)
-    minDate, maxDate = get_dates(sys.argv[1])
-    minId = get_min_id(conn, minDate)
-    maxId = get_max_id(conn, maxDate)
+    minId = get_min_id(conn, minDate, mode)
+    maxId = get_max_id(conn, maxDate, mode)
+    if(mode == "stories" and maxId < minId):
+        print(f"already completed {sys.argv[1]}")
+        sys.exit(0)
     updates = maxId - minId
     print(f"{datetime.now()}: updating {updates} records")
     for id in range(minId, maxId):
         retrieval_time=int(time.time())
-        if is_story(conn, id):
+        if mode=='all' or is_story(conn, id):
             fbdata = retrieve_from_firebase(id)
             update_item(conn, id, fbdata, retrieval_time)
             if (id - minId) % 250 == 0:
