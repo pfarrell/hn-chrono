cf100735b9660baedb338744954cafca7b4bf69f
Patrick Farrell <mr.patfarrell@gmail.com>
2024-11-21 11:39:10 -0500

    add fix for change in values stored for deleted (bool to list)

--- bin/import.py
+++ bin/import.py
@@ -4,7 +4,9 @@ import sqlite3
 
 def dbimport(df, con, table):
     df = df.drop(columns=["timestamp", "ranking", "kids", "parts", "poll"], errors="ignore")
-    df.to_sql(table, con, if_exists="append", index=False, chunksize=5000, method="multi")
+    # rectify some items now being published with a list for deleted instead of a boolean
+#    df['deleted'] = df['deleted'].apply(lambda x: x if isinstance(x, bool) else bool(x))
+    df.to_sql(table, con, if_exists="append", index=False, chunksize=1, method="multi")
 
 
 if __name__ == '__main__':
